a7aba6142faa651797c9adb20197fbc2cce4fd86
Jack Scrivener <10jack222@gmail.com>
2023-08-01 11:26:44 +1200

    it works !!

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 # commented lasted updated on 31/07/2023
 # requires pygame external library
 
-from pickle import TRUE
 import pygame
 import time
 
@@ -14,66 +13,94 @@ import time
 INITAL_SCREEN_HEIGHT = 400
 INITAL_SCREEN_WIDTH = 400
 
-#sets the intial x and y positon of the llama the x is constant however the y will change
-llama_y_pos = 40
+#sets the intial x of the lama and the refrance point for the y
+LLAMA_Y_POS_REF = 200
 LLAMA_X_POS = 20
+
 #sets the time between object spawns in seconds
 OBJECT_SPAWN_INTERVAL = 5
 
-quit_game = True
-
-llama_jump = False
-llama_jump_compleate = True
-llama_duck = False
-llama_duck_compleate = True
-
-llama_jump_period = 3
+#how long a jump takes in seconds
+llama_jump_period = 2
+#how high a jump goes in pixels
 llama_jump_height = 20
 
-llama_duck_period = 3
+#how long a duck takes in seconds
+llama_duck_period = 2
+#how low a duck goes in pixels
 llama_duck_depth = 10
 
-jump_start_time = 4
-duck_start_time = 4
-
-
+#colours for varous objects in rgb values 
 LIGHT_BACROUND = (118,30,138)
 SNAKE_RED = (245,54,34)
+FOOD_BLUE = (127,202,255)
 
-object_list = []
 
+#______________ Non-Changable Varables & obbjects __________________________________________________________________
+#pls leave alone
+
+#creates a list which will be used to store what objects are on screen
+global object_list
+
+
+#takes the time of when the program started to be used for score
 time_since_start = time.time()
+
+#these are used for latching if statments in the llama draw function
+currently_jumping = False
+currently_ducking = False
+
+
+#these are used to keep track of time since a duck or jump started 
+time_duck_started = 0
+time_jump_started = 0
+
+#sets the start state for the main loop
+quit_game = True
+
+#used to set the time intervals for object spawning
+object_counter = 1
 
 #______________ Objects and functions __________________________________________________________________
 
 
 #defines the object class which is used to store info and update the objects which go across the screen
 class objects:
-    #this function is called when the object is created and defines the objects varous varables
-    def __init__(self):
-        #defines the inital varables for the object
+    #defines the inital varables for the object
+    def __inti__(self):
         self.speed = 1
-        self.x_pos = 0
+        self.x_pos = 400
         self.image_name = "test.png"
         self.image_width = 20
         self.image_height = 20
 
-
+    def dict(self):
+        speed_dict = self.speed
+        x_pos = self.x_pos
+        image_name = self.image_name
+        image_width = self.image_width
+        image_height = self.image_height
+    
+        
 
 #This function is called every frame to update the obsticales being displayed
-def  object_update():
-    if time.time() - time_since_start > OBJECT_SPAWN_INTERVAL:
-        object_list.append(objects)
+def  object_update(object_counter):
+    global object_list, k
+    if time.time() - time_since_start > OBJECT_SPAWN_INTERVAL*object_counter:
+        print("if loop started")
+        #object_list.append(objects)
+        object_counter += 1
+    
+    #print("number of objects :{}".format(len(object_list)))
+    
+  
+    return(object_counter)
 
-    for items in object_list:
-        #checks if an object is off the screen and if so delete it 
-        if  object_list[items].x_pos < 0:
-            object_list.remove[items]
+
+def object_draw(object_list):
+    for things in object_list:
+        pygame.draw.rect(screen,SNAKE_RED,[things.x_pos,LLAMA_Y_POS_REF +20,20,20])
         
-        #moves an object to the left by the number of pixels defined by the speed varable
-        object_list[items].x_pos += object_list[items].speed
-
-
 def user_input():
     llama_jump = False
     llama_duck = False
@@ -94,6 +121,58 @@ def user_input():
     return(llama_jump,llama_duck,input_exit)
 
 
+#This function takes two boolen values of weither the user wants to jump or duck. It then checks if they are already
+#jumping and if they are not then initating a jump of height or duck of depth and lenth of time determined by the varables 
+#at the top of the program 
+def llama_draw(currently_jumping,time_jump_started,currently_ducking,time_duck_started):
+    #sets the lammas positon as the refrance positon
+    llama_y_pos = LLAMA_Y_POS_REF
+
+    #checks if the user wants to jump and if they are not already jumping or ducking
+    if llama_jump == True and currently_ducking == False and currently_jumping == False:
+        #If the user wants to jump and is not currently jumping or ducking then it starts a jump
+        currently_jumping = True
+        time_jump_started = time.time()
+
+    #checks if the user wants to duck and if they are not already jumping or ducking
+    if llama_duck == True and currently_ducking == False and currently_jumping == False:
+        #If the user wants to duck and is not currently jumping or ducking then it starts a jump
+        currently_ducking = True
+        time_duck_started = time.time()
+
+    #checks if the jump has gone on longer than the jump period defined at the top of the program
+    if time.time() - time_jump_started > llama_jump_period:
+        #stops the jump
+        currently_jumping = False
+        #sets the y positon to the refrance due to floating points being messey
+        llama_y_pos = LLAMA_Y_POS_REF
+
+    #checks if the duck has gone on longer than the duck period defined at the top of the program
+    if time.time() - time_duck_started > llama_duck_period:
+        #stops the duck
+        currently_ducking = False
+        #sets the y positon to the refrance due to floating points being messey
+        llama_y_pos = LLAMA_Y_POS_REF
+
+    #checks if the llama is currently jumping
+    if  currently_jumping == True:
+        #calculates where the lama should be based on the time using a parabola
+        llama_y_delta = (llama_jump_height)*(time.time() - time_jump_started)*(time.time() - time_jump_started-llama_jump_period)
+        #sets the y positon of the lama to the delta calculated plus the y refrance
+        llama_y_pos = llama_y_delta +LLAMA_Y_POS_REF
+
+    #checks if the llama is currently jumping
+    if  currently_ducking == True:
+        #calculates where the lama should be based on the time using a parabola
+        llama_y_delta = -((llama_duck_depth)*(time.time() - time_duck_started)*(time.time() - time_duck_started-llama_jump_period))
+        #sets the y positon of the lama to the delta calculated plus the y refrance
+        llama_y_pos = llama_y_delta +LLAMA_Y_POS_REF
+
+    pygame.draw.rect(screen,SNAKE_RED,[20,llama_y_pos,20,20])
+
+    return(currently_jumping,time_jump_started,currently_ducking,time_duck_started)
+
+
 
 
 
@@ -104,48 +183,34 @@ def user_input():
 screen = pygame.display.set_mode((INITAL_SCREEN_WIDTH,INITAL_SCREEN_HEIGHT), pygame.RESIZABLE)
 
 
-currently_jumping = False
-time_jump_started = 0
+object_list = [objects,objects]
 
-currently_ducking = False
-time_duck_started = 0
+k = 0
 
-x = 0
-llama_y_delta = 0
+
 while quit_game:
-    llama_jump,llama_duck,quit_game = user_input();
 
-    if llama_jump == True and currently_jumping == False and currently_jumping == False:
-        currently_jumping = True
-        time_jump_started = time.time()
+   # if object_list[0].dict.x_pos < 0:
+    #   print("item deleted")
+     #  object_list.pop([0])
 
-    if llama_duck == True and currently_ducking == False and currently_jumping == False:
-        currently_ducking = True
-        time_duck_started = time.time()
 
-    if time.time() - time_jump_started > 4:
-        currently_jumping = False
-        llama_y_delta = 0
-
-    if time.time() - time_duck_started > 4:
-        currently_ducking = False
-
-    if time.time() - time_jump_started < 4:
-        #llama_y_delta = ((-20)*(time.time() - jump_start_time-1)*(time.time() - jump_start_time-llama_jump_period))
-        x = time.time() - time_jump_started
-        llama_y_delta = (20)*(x-1)*(x-3)
-        llama_y_pos = llama_y_delta +20
-    print("currently_jumping :{}".format(currently_jumping))
-    print("currently_ducking :{}".format(currently_ducking))
-    print("{},{}".format(x,llama_y_delta))
-    print("llama_y_pos:{}".format(llama_y_pos))
-
-    print("")
-    #time.sleep(1)
+    #checks for user inputs
+    llama_jump,llama_duck,quit_game = user_input()
+    
+    #object_counter = object_update(object_counter)
 
     
     screen.fill(LIGHT_BACROUND)
-    #llama_y_delta,llama_jump_compleate,llama_duck_compleate,jump_start_time,duck_start_time = llama_draw(llama_jump,llama_duck,llama_jump_compleate,llama_duck_compleate,jump_start_time,duck_start_time)
-    pygame.draw.rect(screen,SNAKE_RED,[20,llama_y_pos,20,20])
+
+    #object_list = object_draw(object_list)
+    #usses the user inputs to smoothly move the llama through a jump or duck
+    #this function usses latching if statments and relative time hence the nesscity to pass so many varables
+    currently_jumping,time_jump_started,currently_ducking,time_duck_started = llama_draw(currently_jumping,time_jump_started,currently_ducking,time_duck_started)
+    
+
+    pygame.draw.rect(screen,FOOD_BLUE,[0,LLAMA_Y_POS_REF+20,400,20])
+
+    2
 
     pygame.display.update()
